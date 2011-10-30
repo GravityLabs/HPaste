e3430b172243697fdfdd74b490bcc4af6ac5c883
Chris Bissell <chris@gravity.com>
2011-10-30 11:27:52 -0700

    Improvement

--- src/main/scala/com/gravity/hbase/mapreduce/mapreduce2.scala
+++ src/main/scala/com/gravity/hbase/mapreduce/mapreduce2.scala
@@ -19,7 +19,11 @@ import scala.collection.JavaConversions._
 `=,-,-'~~~   `----(,_..'--(,_..'`-.;.'  */
 
 
-
+/**
+* A job encompasses a series of tasks that cooperate to build output.  Each task is usually an individual map or map/reduce operation.
+*
+* To use the job, create a class with a parameterless constructor that inherits HJob, and pass the tasks into the constructor as a sequence.
+*/
 class HJob[S <: SettingsBase](input: HInput, tasks: Seq[HTask[_, _, _, _, S]], output: HOutput) {
   def run(settings: S, conf:Configuration) {
     require(tasks.size > 0, "HJob requires at least one task to be defined")
@@ -78,29 +82,23 @@ class HJob[S <: SettingsBase](input: HInput, tasks: Seq[HTask[_, _, _, _, S]], o
   }
 }
 
-class MyMapper extends HMapReduceTask[LongWritable, Text, BytesWritable, BytesWritable, NullWritable, Text, NoSettings](
-  (hMapContext: HMapContext[LongWritable, Text, BytesWritable, BytesWritable, NoSettings]) => {},
-  (reduceContext: HReduceContext[BytesWritable, BytesWritable, NullWritable, Text, NoSettings]) => {}
-)
-
-//class HMapReduceJob[M <: HMapper[MK,MV,MOK,MOV,S], R <: HReducer[MOK,MOV,ROK,ROV,S], MK,MV,MOK,MOV,ROK,ROV,S <: SettingsBase](mapper:M, reducer: R) {
-//  def run(settings: S) {
-//
-//  }
-//}
-
+/**
+* Base class for initializing the input to an HJob
+*/
 abstract class HInput {
-  def init(conf: Configuration, job: Job) {
-
-  }
+  def init(conf: Configuration, job: Job)
 }
 
+/**
+* Base class for initializing the output from an HJob
+*/
 abstract class HOutput {
-  def init(conf: Configuration, job: Job) {
-
-  }
+  def init(conf: Configuration, job: Job)
 }
 
+/**
+* Initializes input from an HPaste Table
+*/
 case class HTableInput[T <: HbaseTable[T, R], R](table: T) extends HInput {
   override def init(conf: Configuration, job: Job) {
     conf.set(TableInputFormat.INPUT_TABLE, table.tableName)
@@ -108,6 +106,9 @@ case class HTableInput[T <: HbaseTable[T, R], R](table: T) extends HInput {
   }
 }
 
+/**
+* Initializes input from a series of paths.
+*/
 case class HPathInput(paths: Seq[String]) extends HInput {
   override def init(conf: Configuration, job: Job) {
     paths.foreach(path => {
@@ -116,6 +117,9 @@ case class HPathInput(paths: Seq[String]) extends HInput {
   }
 }
 
+/**
+* Outputs to an HPaste Table
+*/
 case class HTableOutput[T <: HbaseTable[T, R], R](table: T) extends HOutput {
   override def init(conf: Configuration, job: Job) {
     conf.set(GravityTableOutputFormat.OUTPUT_TABLE, table.tableName)
@@ -123,6 +127,9 @@ case class HTableOutput[T <: HbaseTable[T, R], R](table: T) extends HOutput {
   }
 }
 
+/**
+* Outputs to an HDFS directory
+*/
 case class HPathOutput(path: String) extends HOutput {
   override def init(conf: Configuration, job: Job) {
     FileSystem.get(conf).delete(new Path(path), true)
@@ -130,6 +137,10 @@ case class HPathOutput(path: String) extends HOutput {
   }
 }
 
+/**
+* This is the input to a task that is in the middle of a job.
+* It reads from the output of the previous task.
+*/
 case class HRandomSequenceInput[K, V]() extends HInput {
   var previousPath: Path = _
 
@@ -140,6 +151,9 @@ case class HRandomSequenceInput[K, V]() extends HInput {
   }
 }
 
+/**
+* This is the output from a task in the middle of a job.  It writes to a sequence temp file
+*/
 case class HRandomSequenceOutput[K, V]() extends HOutput {
   var path = new Path(genTmpFile)
 
@@ -150,7 +164,9 @@ case class HRandomSequenceOutput[K, V]() extends HOutput {
 
 }
 
-
+/**
+* This is a single task in an HJob.  It is usually a single hadoop job (an HJob being composed of several).
+*/
 abstract class HTask[IK, IV, OK, OV, S <: SettingsBase](var input: HInput = HRandomSequenceInput[IK, IV](), var output: HOutput = HRandomSequenceOutput[OK, OV]()) {
   var configuration: Configuration = _
   var settings: S = _
@@ -165,6 +181,7 @@ abstract class HTask[IK, IV, OK, OV, S <: SettingsBase](var input: HInput = HRan
 
     val job = new Job(configuration)
 
+    //If there is a previous HTask, then initialize the input of this task as the output of that task.
     if (previousTask != null && previousTask.output.isInstanceOf[HRandomSequenceOutput[_, _]] && input.isInstanceOf[HRandomSequenceInput[_, _]]) {
       input.asInstanceOf[HRandomSequenceInput[_, _]].previousPath = previousTask.output.asInstanceOf[HRandomSequenceOutput[_, _]].path
     }
@@ -178,6 +195,9 @@ abstract class HTask[IK, IV, OK, OV, S <: SettingsBase](var input: HInput = HRan
   }
 }
 
+/**
+* An HTask that wraps a standard mapper and reducer function.
+*/
 case class HMapReduceTask[MK, MV, MOK: Manifest, MOV: Manifest, ROK, ROV, S <: SettingsBase](mapper: MapperFunc[MK, MV, MOK, MOV, S], reducer: ReducerFunc[MOK, MOV, ROK, ROV, S]) extends HTask[MK, MV, ROK, ROV, S] {
 
   val mapperClass = classOf[HMapper[MK,MV,MOK,MOV,S]]
@@ -198,7 +218,9 @@ case class HMapReduceTask[MK, MV, MOK: Manifest, MOV: Manifest, ROK, ROV, S <: S
   }
 }
 
-
+/**
+* A Task for a mapper-only job
+*/
 case class HMapTask[MK, MV, MOK: Manifest, MOV: Manifest, S <: SettingsBase](mapper: MapperFunc[MK, MV, MOK, MOV, S]) extends HTask[MK, MV, MOK, MOV, S] {
   def decorateJob(conf: Configuration, job: Job) {
     job.setMapperClass(classOf[HMapper[MK, MV, MOK, MOV, S]])
@@ -208,6 +230,9 @@ case class HMapTask[MK, MV, MOK: Manifest, MOV: Manifest, S <: SettingsBase](map
 
 }
 
+/**
+* A task for a Mapper / Combiner / Reducer combo
+*/
 case class HMapCombineReduceTask[MK, MV, MOK: Manifest, MOV: Manifest, ROK, ROV, S <: SettingsBase](mapper: MapperFunc[MK, MV, MOK, MOV, S], combiner: ReducerFunc[MOK, MOV, ROK, ROV, S], reducer: ReducerFunc[MOK, MOV, ROK, ROV, S]) extends HTask[MK, MV, ROK, ROV, S] {
   def decorateJob(conf: Configuration, job: Job) {
     job.setMapperClass(classOf[HMapper[MK, MV, MOK, MOV, S]])
@@ -218,6 +243,10 @@ case class HMapCombineReduceTask[MK, MV, MOK: Manifest, MOV: Manifest, ROK, ROV,
   }
 }
 
+/**
+* This is the class that gets loaded by Hadoop as a mapper.  It delegates the actual mapper functionality back
+* to the HTask that it represents.
+*/
 class HMapper[MK, MV, MOK, MOV, S <: SettingsBase] extends Mapper[MK, MV, MOK, MOV] {
 
   var mapperFunc: MapperFunc[MK, MV, MOK, MOV, S] = _
@@ -245,6 +274,10 @@ class HMapper[MK, MV, MOK, MOV, S <: SettingsBase] extends Mapper[MK, MV, MOK, M
   }
 }
 
+/**
+* This is the actual Reducer that gets loaded by Hadoop.  It delegates the actual reduce functionality back to the
+* HTask that it represents.
+*/
 class HReducer[MOK, MOV, ROK, ROV, S <: SettingsBase] extends Reducer[MOK, MOV, ROK, ROV] {
   var hcontext: HReduceContext[MOK, MOV, ROK, ROV, S] = _
   var context: Reducer[MOK, MOV, ROK, ROV]#Context = _
@@ -270,6 +303,10 @@ class HReducer[MOK, MOV, ROK, ROV, S <: SettingsBase] extends Reducer[MOK, MOV, 
   }
 }
 
+/**
+* This is the context object for a Map function.  It gets passed into the mapper function defined in an HTask.
+* It contains simplified functions for writing values and incrementing counters.
+*/
 class HMapContext[MK, MV, MOK, MOV, S <: SettingsBase](conf: Configuration, counter: (String, Long) => Unit, context: Mapper[MK, MV, MOK, MOV]#Context) extends HContext(conf, counter) {
   def key = context.getCurrentKey
 
@@ -278,6 +315,9 @@ class HMapContext[MK, MV, MOK, MOV, S <: SettingsBase](conf: Configuration, coun
   def write(key: MOK, value: MOV) {context.write(key, value)}
 }
 
+/**
+* This is the context object for a Reduce function.  It gets passed into the reducer defined in an HTask.
+*/
 class HReduceContext[MOK, MOV, ROK, ROV, S <: SettingsBase](conf: Configuration, counter: (String, Long) => Unit, context: Reducer[MOK, MOV, ROK, ROV]#Context) extends HContext(conf, counter) {
   def key = context.getCurrentKey
 
@@ -286,6 +326,9 @@ class HReduceContext[MOK, MOV, ROK, ROV, S <: SettingsBase](conf: Configuration,
   def write(key: ROK, value: ROV) {context.write(key, value)}
 }
 
+/**
+* Base class for contextual objects.  It handles the business for initializing a context properly.
+*/
 class HContext[S <: SettingsBase](conf: Configuration, val counter: (String, Long) => Unit) {
   def apply(message: String, count: Long) {counter(message, count)}
 
@@ -299,26 +342,3 @@ class HContext[S <: SettingsBase](conf: Configuration, val counter: (String, Lon
 
 }
 
-//class JobChain(jobs: Seq[HMapReduceJob]) {
-//
-//  def submit() {
-//
-//  }
-//
-//}
-//
-//class HMapper(mapper: Mapper) {
-//
-//}
-//
-//class HReducer()
-//
-//class HCombiner()
-//
-//class HMapReduceJob(mapper:HMapper, reducer:HReducer, combiner:HCombiner = null) {
-//
-//
-//
-//}
-//
-//class JobChainExample
