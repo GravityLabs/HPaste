e144b43bfa39f77047911b7e9f7f8005e85ea345
Chris Bissell <chris@gravity.com>
2012-05-05 14:31:59 -0700

    Inadvertently changed the contract for the type of Set passed into DeleteOp.

--- src/main/scala/com/gravity/hbase/schema/DeleteOp.scala
+++ src/main/scala/com/gravity/hbase/schema/DeleteOp.scala
@@ -2,6 +2,7 @@ package com.gravity.hbase.schema
 
 import scala.collection.mutable.Buffer
 import org.apache.hadoop.hbase.client.Delete
+import scala.collection.Set
 
 /*             )\._.,--....,'``.
  .b--.        /;   _.. \   _\  (`._ ,.
